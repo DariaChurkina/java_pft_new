4a1b8bdf67f2de96d8fec6c3e70117a8f5e1001b
DariaChurkina <d_churkina@inbox.ru>
2017-03-27 21:46:10 +0300

    Добавлены новые функции

--- sandbox-2/src/main/java/ru/pft/firstjava/HelloWorld.java
+++ sandbox-2/src/main/java/ru/pft/firstjava/HelloWorld.java
@@ -2,6 +2,25 @@ package ru.pft.firstjava;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-}
+		hello("Daria");
+		hello("User");
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " равняется " + area(l));
+
+		double a = 4;
+		double b = 880;
+		System.out.println("Площадь прямоугольника со стороноами " + a + " и " + b + " равняется " + area(a, b));
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + " !");
+	}
+
+	public static double area(double a) {
+		return a*a;
+	}
+
+	public static double area(double a, double b){
+		return a*b;
+	}
 }
